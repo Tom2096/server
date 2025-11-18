4938a572b1bcc51d391ea05a87cba8c2cb867991
Si Yuan He <sy4he@uwaterloo.ca>
2025-11-17 22:46:25 -0800

    fix for summoning

--- gimuserver/gme/handlers/GatchaActionHandler.hpp
+++ gimuserver/gme/handlers/GatchaActionHandler.hpp
@@ -7,7 +7,7 @@ class GatchaActionHandler : public HandlerBase
 {
 public:
 	const char* GetGroupId() const override { return "F7JvPk5H"; }
-	const char* GetAesKey() const override { return "oINq0rfUFPx5MgmT"; }
+	const char* GetAesKey() const override { return "bL9fipzaSy7xN2w1"; }
 	void Handle(UserInfo& user, DrogonCallback cb, const Json::Value& req) const override;
 };
 HANDLER_NS_END
